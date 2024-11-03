aeaf1c8164db65025f8707c2d911c3cde07d0d17
Amit pandey <amitpandeye120@gmail.com>
2024-11-03 22:33:36 +0530

    src1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import react,{useState} from 'react';
 
 function App() {
+  const [data,setData]=useState(null)
+  const [print,setPrint]=useState(false)
+ function getData(val){
+    console.warn(val.target.value)
+    setData(val.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Get Input Box Value :)</h1>
+     {
+      print?
+     <h1>{data}</h1>
+     :null
+          }
+     <input type="text" onChange={getData}></input>
+     <button onClick={()=>setPrint(true)}>Print Data</button>
     </div>
   );
 }
